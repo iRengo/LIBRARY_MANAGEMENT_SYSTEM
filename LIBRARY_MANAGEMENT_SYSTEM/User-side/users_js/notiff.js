31369f8e7d6e266942cfa0efda40add92756563b
Reymart <reymartomega18@gmail.com>
2025-01-09 23:37:05 +0800

    Delete LIBRARY_MANAGEMENT_SYSTEM directory

--- LIBRARY_MANAGEMENT_SYSTEM/User-side/users_js/notiff.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.notify-button input[type="radio"]').forEach((radio) => {
-    radio.addEventListener('change', function () {
-        document.querySelectorAll('.notify-button').forEach(button => button.classList.remove('active'));
-        if (this.checked) {
-            this.closest('.notify-button').classList.add('active');
-        }
-    });
-});
